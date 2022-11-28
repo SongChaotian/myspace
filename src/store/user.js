86bf91d45d75b3e7ad82022011e846caf648c240
‘sct’ <2653325469@qq.com>
2022-11-28 10:50:08 +0800

    first commit

---
+++ src/store/user.js
@@ -0,0 +1,98 @@
+import { setInterval } from 'core-js';
+import $ from 'jquery';
+import jwt_decode from 'jwt-decode';
+
+const ModuleUser = {
+    state: {
+        id: "",
+        username: "",
+        photo: "",
+        followerCount: 0,
+        access: "",
+        refresh: "",
+        is_login: false,
+    },
+    getters: {
+    },
+    mutations: {
+        updateUser(state, user) {  // 更新用户信息
+            state.id = user.id;
+            state.username = user.username;
+            state.photo = user.photo;
+            state.followerCount = user.followerCount;
+            state.access = user.access;
+            state.refresh = user.refresh;
+            state.is_login = user.is_login;
+        },
+
+        updateAccess(state, access) {
+            state.access = access;
+        },
+
+        logout(state) {
+            state.id = "";
+            state.username = "";
+            state.photo = "";
+            state.followerCount = 0;
+            state.access = "";
+            state.refresh = "";
+            state.is_login = false;
+        }
+    },
+    actions: {
+        login(context, data) {
+            $.ajax({
+                url: 'https://app165.acapp.acwing.com.cn/api/token/',
+                type: "post",
+                data: {
+                    username: data.username,
+                    password: data.password,
+                },
+                success(resp) {
+                    const { access, refresh } = resp;
+                    const access_obj = jwt_decode(access);
+
+                    setInterval(() => {
+                        $.ajax({
+                            url: 'https://app165.acapp.acwing.com.cn/api/token/refresh/',
+                            type: "post",
+                            data: {
+                                refresh: refresh,
+                            },
+                            success(resp) {
+                                context.commit("updateAccess", resp.access);
+                            }
+                        });
+                    }, 4.5 * 60 * 1000);
+
+                    $.ajax({
+                        url: 'https://app165.acapp.acwing.com.cn/myspace/getinfo/',
+                        type: "get",
+                        data: {
+                            user_id: access_obj.user_id,
+                        },
+                        headers: {  // JWT验证
+                            'Authorization': "Bearer " + access,
+                        },
+                        success(resp) {
+                            context.commit("updateUser", {
+                                ...resp,
+                                access: access,
+                                refresh: refresh,
+                                is_login: true,
+                            });
+                            data.success();
+                        }
+                    });
+                },
+                error() {
+                    data.error();
+                }
+            });
+        }
+    },
+    modules: {
+    }
+};
+
+export default ModuleUser;
